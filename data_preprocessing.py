efdf4fce626f59bda1079e27d3c0974074d6bcba
Riya Goel <55539590+riyagoel192@users.noreply.github.com>
2020-03-27 18:36:12 +0530

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#importing libraries
+import numpy as np      #contains mathematical tools
+import pandas as pd     #used to read and import datasets
+import matplotlib.pyplot as plt   #helps plot charts
+
+#importing the dataset
+
